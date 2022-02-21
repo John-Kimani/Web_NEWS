340aeb42bed8180bf2daa4fd8b3f58ab4906446d
John-Kimani <kimwaweru100@gmail.com>
2022-02-21 20:05:31 +0300

    fix: top stories can be fetched

--- src/app/services/newsapiservice.service.ts
+++ src/app/services/newsapiservice.service.ts
@@ -5,21 +5,27 @@ import { HttpClient } from '@angular/common/http'; //enables communication with 
   providedIn: 'root'
 })
 export class NewsapiserviceService {
-    //insert bbc url link from newsAPI
-    newsApiurl = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=eeee21a4090644c3b184587ca6b8df35"
+   
+  newsApiurlOne: any;
+  newsAPIurlTwo: any;
+  newsAPIkey:any;
+  topStories: any;
 
   constructor( private http:HttpClient) { 
+
+    this.newsApiurlOne = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=eeee21a4090644c3b184587ca6b8df35"
+    this.topStories = []
 
   }
   fetchTopStoriesBBC(){
    //using promises
    let topStory = new Promise((resolve,reject)=>{
-     this,this.http.get<any>(this.newsApiurl).toPromise().then(
+     this,this.http.get<any>(this.newsApiurlOne).toPromise().then(
        response => {
-
+          this.topStories = response.article
         resolve(console.log('Top stories can be fetched'))
        }, error =>{
-         reject(console.log('Top stories cant be fetched'))
+         reject(error)
        }
      )
    })
