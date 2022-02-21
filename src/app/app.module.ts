b026f67f09d0bd1f9ac7d8032bbcf78db39ea7fd
John-Kimani <kimwaweru100@gmail.com>
2022-02-21 19:28:28 +0300

    creates and configures news service and imports httpclient module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http'; //imports https client module
+import { NewsapiserviceService } from './services/newsapiservice.service'; // imports news service
+
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { TopstoryComponent } from './components/topstory/topstory.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TopstoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NewsapiserviceService], //mention service as provider
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/components/topstory/topstory.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-topstory',
+  templateUrl: './topstory.component.html',
+  styleUrls: ['./topstory.component.css']
+})
+export class TopstoryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
