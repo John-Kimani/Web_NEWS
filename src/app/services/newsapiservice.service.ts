142a79eda308c76e2916f71e217af5e27deb2d6e
John-Kimani <kimwaweru100@gmail.com>
2022-02-24 16:40:31 +0300

    Feat: Displays response fecthed on console

--- src/app/services/newsapiservice.service.ts
+++ src/app/services/newsapiservice.service.ts
@@ -1,34 +1,36 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'; //enables communication with the server
+import { Article } from '../classes/article'; // imports class with all its defined instances
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsapiserviceService {
    
-  newsApiurlOne: any;
-  newsAPIurlTwo: any;
-  newsAPIkey:any;
-  topStories: any;
+  newsApiurlOne = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=eeee21a4090644c3b184587ca6b8df35"
+  topStory = new Article('','','','') //creates new article instance
+  topStories:any[] = [];
+  // static topStories: never[]; //quick fix recommendation
 
   constructor( private http:HttpClient) { 
-
-    this.newsApiurlOne = "https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=eeee21a4090644c3b184587ca6b8df35"
-    this.topStories = []
 
   }
   fetchTopStoriesBBC(){
    //using promises
-   let topStory = new Promise((resolve,reject)=>{
-     this,this.http.get<any>(this.newsApiurlOne).toPromise().then(
+   let promise = new Promise((resolve,reject)=>{
+     this.http.get<any>(this.newsApiurlOne).toPromise().then(
        response => {
-          this.topStories = response.article
-        resolve(console.log('Top stories can be fetched'))
+          this.topStory = new Article('','','','')
+          this.topStory.article = response.article
+          this.topStory.title = response.title
+          this.topStory.description = response.description
+          // this.topStories.push(this.topStory)
+        resolve(console.log('Top stories can be fetched' + JSON.stringify(response.articles[0])))
        }, error =>{
          reject(error)
        }
      )
    })
-   return topStory   
+   return promise   
   }
 }
